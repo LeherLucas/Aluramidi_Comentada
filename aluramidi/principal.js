25bdbf337d92575546a0650326b940a80e2f67c8
LeherLucas <127355311+LeherLucas@users.noreply.github.com>
2023-08-27 12:22:08 -0300

    Add files via upload

---
+++ aluramidi/principal.js
@@ -0,0 +1,4 @@
+document.querySelector('.tecla_pom'); /*Aqui utilizamos a função document. para acessar
+o documento HTML, depois o querySelector para selecionar a classe desejada para futuras
+alterações */
+
